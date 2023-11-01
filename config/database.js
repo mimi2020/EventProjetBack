c57c8c56f24bb458304efa00ada18ca39ed69c13
rim chebbi <rim.chabbi@gmail.com>
2023-11-01 11:41:35 +0100

    add new DB

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const {connect} = require('mongoose');
 const{error,success} = require("consola");
-var DB="mongodb://127.0.0.1/evenements";
-
+const { MongoClient } = require('mongodb');
+// var DB="mongodb://127.0.0.1/evenements";
+var DB="mongodb+srv://test1:test1@cluster0.moznla4.mongodb.net/rimtest?retryWrites=true&w=majority";
 const connectDB = async () =>{
 
     try{
@@ -21,3 +22,29 @@ const connectDB = async () =>{
 };
 
 module.exports = connectDB();
+
+// async function main(){
+//     /**
+//      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+//      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+//      */
+//     const uri = "mongodb+srv://test1:test1@cluster0.moznla4.mongodb.net/rimtest?retryWrites=true&w=majority";
+
+
+//     const client = new MongoClient(uri);
+
+//     try {
+//         // Connect to the MongoDB cluster
+//         await client.connect();
+
+//         // Make the appropriate DB calls
+//     // await  listDatabases(client);
+
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
+
+// main().catch(console.error);
